5436ecb2ff1ca835e0d5f4809cd936a46dbb1075
Ryan Allen <ryan@ryangallen.com>
2015-04-16 01:10:56 -0400

    move pet list to directive and and pet details directive

--- animals/static/js/controllers.js
+++ animals/static/js/controllers.js
@@ -1,41 +1,5 @@
 var petTrackerControllers = angular.module('petTrackerControllers', []);
 
 petTrackerControllers.controller('PetTrackerController', [
-    '$scope',
-    '$q',
-    'PetTrackerFactory',
-    function($scope, $q, PetTrackerFactory){
-        $scope.ordering = "id";
-        $scope.order = function(prop){
-            $scope.ordering = $scope.ordering == prop ? "-" + prop : prop
-        }
-
-        $scope.filterAnimal = function(pet){
-            return pet.breed.animal.showing;
-        }
-
-        var animalsPromise = PetTrackerFactory.readAnimals().then(
-            function(promise){$scope.animals = promise.data.results}
-        );
-        var breedsPromise = PetTrackerFactory.readBreeds().then(
-            function(promise){$scope.breeds = promise.data.results}
-        );
-        var petsPromise = PetTrackerFactory.readPets().then(
-            function(promise){$scope.pets = promise.data.results}
-        );
-
-        $q.all([animalsPromise, breedsPromise, petsPromise]).then(function(){
-            _.each($scope.animals, function(animal){
-                animal.showing = true;
-                var animalBreeds = _.where($scope.breeds, {animal: animal.id});
-                _.each(animalBreeds, function(breed){
-                    breed.animal = animal;
-                    var breedPets = _.where($scope.pets, {breed: breed.id});
-                    _.each(breedPets, function(pet){
-                        pet.breed = breed;
-                    });
-                });
-            });
-        });
-    }
+    '$scope', function($scope, $q, PetTrackerFactory){}
 ]);
--- animals/static/js/directives.js
+++ animals/static/js/directives.js
@@ -1,1 +1,64 @@
 var petTrackerDirectives = angular.module('petTrackerDirectives', [])
+
+petTrackerDirectives
+    .directive('petList', [
+        '$q',
+        'PetTrackerFactory',
+        function($q, PetTrackerFactory){
+            return {
+                restrict: 'E',
+                templateUrl: "/static/js/templates/pet_list.html",
+                replace: true,
+                link: function(scope){
+                    scope.ordering = "id";
+                    scope.order = function(prop){
+                        scope.ordering = scope.ordering == prop ? "-" + prop : prop
+                    }
+
+                    scope.filterAnimal = function(pet){
+                        return pet.breed.animal.showing;
+                    }
+
+                    var animalsPromise = PetTrackerFactory.readAnimals().then(
+                        function(promise){scope.animals = promise.data.results}
+                    );
+                    var breedsPromise = PetTrackerFactory.readBreeds().then(
+                        function(promise){scope.breeds = promise.data.results}
+                    );
+                    var petsPromise = PetTrackerFactory.readPets().then(
+                        function(promise){scope.pets = promise.data.results}
+                    );
+
+                    $q.all([animalsPromise, breedsPromise, petsPromise]).then(function(){
+                        _.each(scope.animals, function(animal){
+                            animal.showing = true;
+                            var animalBreeds = _.where(scope.breeds, {animal: animal.id});
+                            _.each(animalBreeds, function(breed){
+                                breed.animal = animal;
+                                var breedPets = _.where(scope.pets, {breed: breed.id});
+                                _.each(breedPets, function(pet){
+                                    pet.breed = breed;
+                                });
+                            });
+                        });
+                    });
+
+                    scope.showPetDetails = function(pet){
+                        scope.focusedPet = pet;
+                    }
+                }
+            }
+        }
+    ])
+    .directive('petDetails', ['PetTrackerFactory',
+        function(PetTrackerFactory){
+            return {
+                restrict: 'E',
+                templateUrl: "/static/js/templates/pet_details.html",
+                replace: true,
+                link: function(scope){
+                    console.log('howdy');
+                }
+            }
+        }
+    ]);
